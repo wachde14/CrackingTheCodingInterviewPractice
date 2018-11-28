a98431f76847931b8387316696688df67fda03d0
wachde14 <41805653+wachde14@users.noreply.github.com>
2018-11-27 21:07:03 -0700

    Add 8.1

--- Chapter 8 - Recursion and Dynamic Programming/PracticeProblems.cs
+++ Chapter 8 - Recursion and Dynamic Programming/PracticeProblems.cs
@@ -9,9 +9,27 @@ namespace Chapter_8___Recursion_and_Dynamic_Programming
     public class PracticeProblems
     {
         #region 8.1
-        public int Problem_8_1(int numberOfSteps)
+        public int Problem_8_1(int targetSteps)
         {
-            return 3;
+            return CountSteps(0, targetSteps);
+        }
+
+        public int CountSteps(int stepsMoved, int targetSteps)
+        {
+            if (stepsMoved == targetSteps)
+            {
+                return 1;
+            }
+            
+            if (stepsMoved > targetSteps)
+            {
+                return 0;
+            }
+
+            return CountSteps(stepsMoved + 1, targetSteps)
+                + CountSteps(stepsMoved + 2, targetSteps)
+                + CountSteps(stepsMoved + 3, targetSteps);
+
         }
         #endregion
     }
