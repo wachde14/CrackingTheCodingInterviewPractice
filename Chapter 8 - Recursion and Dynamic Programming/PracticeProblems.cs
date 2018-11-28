124dc8639909034c40ffab0a6bfbd751b885dfa1
wachde14 <41805653+wachde14@users.noreply.github.com>
2018-11-27 18:13:18 -0700

    Add Chapter 8 project

---
+++ Chapter 8 - Recursion and Dynamic Programming/PracticeProblems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_8___Recursion_and_Dynamic_Programming
+{
+    public class PracticeProblems
+    {
+        #region 8.1
+        public int Problem_8_1(int numberOfSteps)
+        {
+            return 3;
+        }
+        #endregion
+    }
+}
