f31876698c251c99c2e95aa6ad600d11659ed50e
Derek <41805653+wachde14@users.noreply.github.com>
2019-02-12 16:19:27 -0700

    Finish 7.4

--- Chapter 7 - Object-Oriented Design/7_4_ParkingLot.cs
+++ Chapter 7 - Object-Oriented Design/7_4_ParkingLot.cs
@@ -1,13 +1,186 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace Chapter_7___Object_Oriented_Design
 {
     /// <summary>
     /// Parking Lot: Design a parking lot using object-oriented principles.
-    /// </summary>
-    class _7_4_ParkingLot
+    /// • The parking lot has multiple levels. Each level has multiple rows of spots.
+    /// • The parking lot can park motorcycles, cars, and buses.
+    /// • The parking lot has motorcycle spots, compact spots, and large spots.
+    /// • A motorcycle can park in any spot.
+    /// • A car can park in either a single compact spot or a single large spot.
+    /// • A bus can park in five large spots that are consecutive and within the same row. It cannot park in small spots.
+    /// /// </summary>
+    public class LargeSpotSection
     {
+        private int numberOfOpenSpots;
+        public int[] largeSpots;
 
+        public LargeSpotSection(int size)
+        {
+            numberOfOpenSpots = size;
+            largeSpots = new int[size];
+
+            for (int i = 0; i < largeSpots.Length; i++)
+            {
+                largeSpots[i] = -1;
+            }
+        }
+
+        public bool CanParkCar()
+        {
+            return numberOfOpenSpots > 0;
+        }
+
+        public bool CanParkBus()
+        {
+            if (numberOfOpenSpots <= 4)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < largeSpots.Length; i++)
+            {
+                //Sliding Window to check for 5 consecutive spots
+
+            }
+
+            return false;
+        }
+
+        public void ReorganizeBusParking()
+        {
+            //"Defrag" the largeSpots array for more optimal parking
+        }
+    }
+
+    public class ParkingLot
+    {
+        public int motorcycleSpotsAvailable;
+        public int compactSpotsAvailable;
+        public List<LargeSpotSection> largeSpotSectionList;
+
+        public bool CanParkMotorcycle()
+        {
+            if (motorcycleSpotsAvailable <= 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool CanParkCar()
+        {
+            if (compactSpotsAvailable > 0)
+            {
+                return true;
+            }
+
+            foreach (LargeSpotSection largeSpot in largeSpotSectionList)
+            {
+                if (largeSpot.CanParkCar())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool CanParkBus()
+        {
+            foreach (LargeSpotSection largeSpot in largeSpotSectionList)
+            {
+                if (largeSpot.CanParkBus())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class ParkingLotComplex
+    {
+        private readonly List<ParkingLot> _floors;
+
+        public ParkingLotComplex(List<ParkingLot> inputFloors)
+        {
+            _floors = inputFloors;
+        }
+
+        void Park(Motorcycle motorcycle)
+        {
+            foreach (ParkingLot parkingLot in _floors)
+            {
+                if (parkingLot.CanParkMotorcycle())
+                {
+                    //Park motorcycle by a unique identifier
+                    return;
+                }
+            }
+
+            throw new Exception("Cannot park motorcycle");
+        }
+
+        void Park(Car car)
+        {
+            foreach (ParkingLot parkingLot in _floors)
+            {
+                if (parkingLot.CanParkCar())
+                {
+                    //Park car by a unique identifier
+                    return;
+                }
+            }
+
+            throw new Exception("Cannot park car");
+        }
+
+        void Park(Bus bus)
+        {
+            foreach (ParkingLot parkingLot in _floors)
+            {
+                if (parkingLot.CanParkBus())
+                {
+                    //Park bus by a unique identifier
+                    return;
+                }
+            }
+
+            throw new Exception("Cannot park bus");
+        }
+    }
+
+    public abstract class Vehicle
+    {
+        public string LicensePlateNumber;
+        public string DriverName;
+    }
+
+    public class Car : Vehicle
+    {
+        public Car()
+        {
+        }
+    }
+
+    public class Bus : Vehicle
+    {
+        public Bus()
+        {
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle()
+        {
+        }
     }
 
     public class _7_4_ParkingLotTests
---
+++ Chapter 7 - Object-Oriented Design/7_5_OnlineBookReader.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Chapter_7___Object_Oriented_Design
+{
+    /// <summary>
+    /// Online Book Reader: Design the data structures for an online book reader system.
+    /// • User membership creation and extension.
+    /// • Searching the database of books.
+    /// • Reading a book.
+    /// • Only one active user at a time
+    /// • Only one active book by this user.
+    /// </summary>
+    class _7_5_OnlineBookReader
+    {
+    }
+
+    public class _7_5_OnlineBookReaderTests
+    {
+        [Test]
+        public void _7_5_OnlineBookReader_With_Should()
+        {
+            //Arrange
+        }
+    }
+}
